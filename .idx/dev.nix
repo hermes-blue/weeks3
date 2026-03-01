1c0032c6ad1e1ba59fc06f3801d84726d534f4e2
jiralboy <jiralboy@gmail.com>
2026-03-01 06:37:38 +0000

    chore: Set up local testing environment for Cloudflare Functions with wrangler and concurrently

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -29,7 +29,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
+          command = ["npm" "run" "dev:all" "--" "--port" "$PORT" "--host" "0.0.0.0"];
           manager = "web";
         };
       };
